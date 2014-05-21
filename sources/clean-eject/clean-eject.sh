9fcbd4551e5034fa6fc1e88559c83da88460f41f
Daniel Kriesten <krid@tu-chemnitz.eu>
2014-05-21 14:16:02 +0200

    fixes: correct query, use hdiutil, quiet find

--- sources/clean-eject/clean-eject.sh
+++ sources/clean-eject/clean-eject.sh
@@ -3,13 +3,17 @@
 DRIVE="{query}"
 
 # hack to allow running from commandline
-if [[ "${DRIVE}" = "" ]]; then
+if [[ "${DRIVE}" = "{query}" ]]; then
   DRIVE="${1}"
+fi
+
+if [[ "$DRIVE" = "" ]]; then
+  exit 1
 fi
 
 dot_clean -m --keep=mostrecent "${DRIVE}"
 
-find "${DRIVE}" -name ".DS_Store" -exec rm {} \;
+find "${DRIVE}" -name ".DS_Store" -exec rm {} \; 2>/dev/null
 
 rm -f "${DRIVE}"/.VolumeIcon.icns
 rm -Rf "${DRIVE}"/.fseventsd
@@ -17,11 +21,15 @@ rm -Rf "${DRIVE}"/.TemporaryItems
 rm -Rf "${DRIVE}"/.Spotlight-V100
 rm -Rf "${DRIVE}"/.Trashes
 
-RET=$(diskutil eject "${DRIVE}" 2>&1)
+# seems to only work for disks
+#RET=$(diskutil eject "${DRIVE}" 2>&1)
+RET=$(hdiutil detach "${DRIVE}" 2>&1)
 
 if [[ $? -ne 0 ]]; then
   echo "${RET}"
   exit 1
 fi
+
+echo "${DRIVE}"
 
 exit 0
