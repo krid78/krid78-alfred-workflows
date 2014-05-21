ff6171e647bdcdd47f85d5969930a6690a5db9b8
Daniel Kriesten <krid@tu-chemnitz.eu>
2014-05-21 15:01:16 +0200

    fix: misspelled xml line
    and improved readability of output

--- sources/clean-eject/filter-disks.sh
+++ sources/clean-eject/filter-disks.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# File          :  filter-disks.sh
 # Author(s)     :  Daniel Kriesten
 # Email         :  daniel.kriesten@etit.tu-chemnitz.de
 # Creation Date :  Mi 21 Mai 11:48:22 2014
 ########################################################################
+
+QUERY="{query}"
 
 # comma seperated list of fs-types, e.g msdos,afpfs
 FSTYPE_LIST="msdos,afpfs"
@@ -11,10 +12,10 @@ FSTYPE_LIST="msdos,afpfs"
 MOUNTLIST=$(mount -t ${FSTYPE_LIST} 2>/dev/null |while read i; do echo ${i% \(*}; done)
 
 # Initial for XML
-OUTPUT="<?xml version=>\"1.0\"?>"
+OUTPUT="<?xml version=\"1.0\"?>"
 
 # the beginning
-OUTPUT+="<items>"
+OUTPUT+="\n<items>"
 
 # this seems ugly, but helps us reading mount points with spaces
 OIFS="${IFS}"
@@ -23,15 +24,14 @@ IFS="
 for MNT in ${MOUNTLIST}; do
   IFS=";" read -ra MNTPKT <<< "${MNT/ on /;}"
   THEMOUNTPOINT="${MNTPKT[1]##*/}"
-  OUTPUT="${OUTPUT}""<item uid=\"${THEMOUNTPOINT/ /}\" arg=\"${MNTPKT[1]}\" valid=\"yes\"><title>${THEMOUNTPOINT}</title><subtitle>${MNTPKT[0]} mounted at ${MNTPKT[1]}</subtitle></item>"
+  OUTPUT="${OUTPUT}""\n<item uid=\"${THEMOUNTPOINT/ /}\" arg=\"${MNTPKT[1]}\" valid=\"yes\">\n<title>${THEMOUNTPOINT}</title>\n<subtitle>${MNTPKT[0]} mounted at ${MNTPKT[1]}</subtitle>\n</item>"
 done
 IFS="${OIFS}"
 
 # the end
-OUTPUT+="</items>"
+OUTPUT+="\n</items>"
 
 echo -e ${OUTPUT}
 
 # vim: ts=2:sw=2:tw=80:fileformat=unix
 # vim: comments& comments+=b\:# formatoptions& formatoptions+=or
-
